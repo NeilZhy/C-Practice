378a281f61d9dce687752c2cf16ed4b99513bc37
unknown <NeilZhy@163.com>
2016-11-29 23:42:28 +0800

    字符替换

---
+++ StringSubstitution.c
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+
+void CharactersToReplace(char str[])
+{
+	int i = 0,j = 0;
+	while (1)
+	{
+		if ('\0' != str[i])
+		{
+			if (' ' == str[i])
+			{
+				str = (char *)realloc(str, 3 * sizeof(char));   //Re open storage space
+				for (j = sizeof(str)+1; j > i; j--)       //Move three spaces after the space, in order to prepare for the replacement of the front
+				{
+					str[j + 3] = str[j];
+				}
+				str[i] = '%';
+				str[i + 1] = '2';
+				str[i + 2] = '0';
+			}
+		}
+		else
+		{
+			printf("\n");
+			break;
+		}
+		i++;
+	}
+}
+
+
+
+void PrintToReplace(char str[])
+{
+	int i = 0;
+	while (1)
+	{
+		if ('\0' != str[i])
+		{
+			if (' ' == str[i])
+				printf("%%20");
+			else
+				printf("%c", str[i]);
+		}
+		else
+		{
+			printf("\n");
+			break;
+		}
+			
+		i++;
+	}
+}
+
+int main()
+{
+	char str[] = "We are happy.";
+	CharactersToReplace(str);
+	PrintToReplace(str);
+	return 0;
+}

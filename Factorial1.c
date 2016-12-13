9106b7c19b78c636927c9e662dd00c7b72315ad9
unknown <NeilZhy@163.com>
2016-12-13 11:44:32 +0800

    递归实现阶乘

---
+++ Factorial1.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<windows.h>
+
+unsigned int Factorial(unsigned int num)
+{
+	if (1 == num)
+		return 1;
+	return num * Factorial(num -1);
+}
+
+int main()
+{
+	unsigned int number = 0;
+	printf("Please enter the order you need to seek the multiplier\n");
+	scanf("%u",&number);
+	printf("%u\n", Factorial(number));
+	system("pause");
+	return 0;
+}
+
+//”√µ›πÈ µœ÷Ω◊≥À

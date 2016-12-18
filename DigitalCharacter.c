592308caff4f4f5a225fa1d2c128a2e49f3a2ec9
unknown <NeilZhy@163.com>
2016-12-18 08:38:46 +0800

    哈哈

---
+++ DigitalCharacter.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<windows.h>
+
+void DigitalCharacter(int unsigned num)
+{
+	int unsigned q = num / 10;         //一开始的时候我直接写的是num /= 10,这样的话就没有办法取到最后一个数字了，通常情况下引入一个变量
+	if (q != 0)
+	{
+		DigitalCharacter(q);
+	}
+	printf("%u\t",num % 10);
+}
+
+int main()
+{
+	int unsigned num = 0;
+	printf("Please enter a non negative integer\n");
+	scanf("%d",&num);
+	DigitalCharacter(num);
+	printf("\n");
+	system("pause");
+	return 0;
+}
+
+//print out each digit of a number

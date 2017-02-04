f92f61f57e119b99ec1f2dad35dd09b05ab68ab7
NeilZhy <NeilZhy@163.com>
2017-02-04 22:30:27 +0800

    打印C

---
+++ PrintfC.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<windows.h>
+
+int main()
+{
+	printf("Hello C-world!\n");
+	printf("****\n");
+	printf("*\n");
+	printf("*\n");
+	printf("****\n");
+	system("pause");
+	return 0;
+}

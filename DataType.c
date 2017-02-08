ae4034f0270032a48c8c7028aa4705465492b242
NeilZhy <NeilZhy@163.com>
2017-02-08 13:59:24 +0800

    测试数据类型的大小

---
+++ DataType.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<windows.h>
+
+int main()
+{
+	printf("char   %d\n",sizeof(char));
+	printf("int   %d\n", sizeof(int));
+	printf("long   %d\n", sizeof(long));
+	printf("long long   %d\n", sizeof(long long));
+	printf("float   %d\n", sizeof(float));
+	printf("double   %d\n", sizeof(double));
+	printf("long double   %d\n", sizeof(long double));
+	printf("char   %d\n", sizeof(char));
+	printf("char   %d\n", sizeof(char));
+	printf("char   %d\n", sizeof(char));
+	system("pause");
+	return 0;
+}

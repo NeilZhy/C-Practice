08398cee352deac1bbc3ee02ce741ae3a9ff351f
NeilZhy <NeilZhy@163.com>
2017-02-11 17:56:41 +0800

    静态变量

---
+++ StaticVariable.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<windows.h>
+
+void test()
+{
+	//int num = 0;                //按这种方式定义num时，每次调用这个函数的时候，num都会被重新定义，所以输出的结果是是个1
+	static int num = 0;           //这里我把num定义成静态的，它就是在内存中被唯一创建，输出的结果是1--10
+	num++;
+	printf("%d  ",num);
+}
+
+int main()
+{
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		test();
+	}
+	printf("\n");
+	system("pause");
+	return 0;
+}
